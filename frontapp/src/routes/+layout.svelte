8e2db6508ae73bd1830ed820c093bef61d3c0c98
PILSUNyou <dbyo1125@naver.com>
2024-07-01 16:28:57 +0900

    static routing

--- frontapp/src/routes/+layout.svelte
+++ frontapp/src/routes/+layout.svelte
@@ -3,4 +3,8 @@
 </script>
 
 <div class="text-orange-600">레이아웃</div>
+<a href="/">메인으로</a>
+<a class="text-blue-600" href="/articles">게시글 목록</a>
+<a class="text-red-600" href="/test">테스트 목록</a>
+
 <slot />
