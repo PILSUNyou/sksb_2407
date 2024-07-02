e0327a43836486adbf7fe46c1156f24828a51912
PILSUNyou <dbyo1125@naver.com>
2024-07-02 16:49:18 +0900

    set context auth

--- src/main/java/com/example/sksb/domain/member/service/MemberService.java
+++ src/main/java/com/example/sksb/domain/member/service/MemberService.java
@@ -4,13 +4,18 @@ import com.example.sksb.domain.member.entity.Member;
 import com.example.sksb.domain.member.repository.MemberRepository;
 import com.example.sksb.global.exceptions.GlobalException;
 import com.example.sksb.global.rsData.RsData;
+import com.example.sksb.global.security.SecurityUser;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -39,6 +44,7 @@ public class MemberService {
         return passwordEncoder.matches(password, member.getPassword());
     }
 
+
     @AllArgsConstructor
     @Getter
     public static class AuthAndMakeTokensResponseBody {
@@ -63,5 +69,20 @@ public class MemberService {
                 "로그인 성공",
                 new AuthAndMakeTokensResponseBody(member, accessToken, refreshToken)
         );
+    }
+
+    public SecurityUser getUserFromAccessToken(String accessToken) {
+        Map<String, Object> payloadBody = authTokenService.getDataFrom(accessToken);
+
+        long id = (int) payloadBody.get("id");
+        String username = (String) payloadBody.get("username");
+        List<String> authorities = (List<String>) payloadBody.get("authorities");
+
+        return new SecurityUser(
+                id,
+                username,
+                "",
+                authorities.stream().map(SimpleGrantedAuthority::new).toList()
+        ); // SimpleGrantedAuthority: 사용자의 권한같은걸 표현 .. ?
     }
 }
